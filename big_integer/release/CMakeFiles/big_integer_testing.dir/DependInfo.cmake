e144f76a4cf487d42b0d2854edc8d8811fee3197
Zahar Koval <ellesterate@gmail.com>
2018-03-29 18:06:10 +0300

    Travis CI

---
+++ big_integer/release/CMakeFiles/big_integer_testing.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/starorpheus/Devel/cpp_course/big_integer/big_integer.cpp" "/home/starorpheus/Devel/cpp_course/big_integer/release/CMakeFiles/big_integer_testing.dir/big_integer.cpp.o"
+  "/home/starorpheus/Devel/cpp_course/big_integer/big_integer_testing.cpp" "/home/starorpheus/Devel/cpp_course/big_integer/release/CMakeFiles/big_integer_testing.dir/big_integer_testing.cpp.o"
+  "/home/starorpheus/Devel/cpp_course/big_integer/gtest/gtest-all.cc" "/home/starorpheus/Devel/cpp_course/big_integer/release/CMakeFiles/big_integer_testing.dir/gtest/gtest-all.cc.o"
+  "/home/starorpheus/Devel/cpp_course/big_integer/gtest/gtest_main.cc" "/home/starorpheus/Devel/cpp_course/big_integer/release/CMakeFiles/big_integer_testing.dir/gtest/gtest_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
