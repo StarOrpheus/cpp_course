efda67b622d0c289664ab84794b96574a894570d
Zahar Koval <ellesterate@gmail.com>
2018-03-31 13:05:19 +0300

    Move semantics added

--- big_integer/big_integer.cpp
+++ big_integer/big_integer.cpp
@@ -9,13 +9,18 @@
 big_integer::big_integer() : big_integer(0) {}
 
 big_integer::big_integer(big_integer const &x) {
-    this->data = x.data;
+    this->m_data = x.m_data;
     this->sign = x.sign;
 }
 
-big_integer::big_integer(int x) : data(1, (uint32_t) abs(x)), sign(x >= 0) {}
+big_integer::big_integer(big_integer && x) {
+    this->m_data = std::move(x.m_data);
+    this->sign = x.sign;
+}
 
-big_integer::big_integer(uint32_t x) : data(1, x), sign(true) {}
+big_integer::big_integer(int x) : m_data(1, std::abs(x)), sign(x >= 0) {}
+
+big_integer::big_integer(uint32_t x) : m_data(1, x), sign(true) {}
 
 big_integer::big_integer(std::string const &str) {
     if (str.length() == 0) {
@@ -39,10 +44,17 @@ big_integer::big_integer(std::string const &str) {
         tmp += str[i] - '0';
     }
 
-    data = std::move(tmp.data);
+    m_data = std::move(tmp.m_data);
     if (!tmp.is_zero()) {
         this->sign = sign;
     }
+}
+
+big_integer& big_integer::operator=(big_integer && other) {
+    this->m_data = std::move(other.m_data);
+    this->sign = other.sign;
+
+    return *this;
 }
 
 big_integer &big_integer::operator+=(big_integer const &rhs) {
@@ -50,14 +62,14 @@ big_integer &big_integer::operator+=(big_integer const &rhs) {
         uint32_t carry = 0;
         for (size_t i = 0; i < std::max(length(), rhs.length()) || carry; i++) {
             if (i == length()) {
-                data.push_back(0);
+                m_data.push_back(0);
             }
 
-            uint64_t tmp = data[i];
-            tmp += ((uint64_t) 1) * carry + (i < rhs.length() ? rhs.data[i] : 0);
+            uint64_t tmp = m_data[i];
+            tmp += ((uint64_t) 1) * carry + (i < rhs.length() ? rhs.m_data[i] : 0);
             carry = (tmp >> 32);
-//            data[i] = (tmp << 32) >> 32;
-            data[i] = tmp;
+//            m_data[i] = (tmp << 32) >> 32;
+            m_data[i] = tmp;
         }
     } else {
         bool s1 = sign;
@@ -71,28 +83,28 @@ big_integer &big_integer::operator+=(big_integer const &rhs) {
         if (*this >= trhs) {
             int carry = 0;
             for (size_t i = 0; i < trhs.length() || carry; i++) {
-                int64_t tmp = data[i];
+                int64_t tmp = m_data[i];
                 tmp -= carry;
-                tmp -= (i < trhs.length() ? trhs.data[i] : 0);
+                tmp -= (i < trhs.length() ? trhs.m_data[i] : 0);
                 carry = tmp < 0;
                 if (carry) tmp += ((int64_t) 1ll << 32);
-                data[i] = tmp;
+                m_data[i] = tmp;
             }
 
             sign = s1;
         } else {
             int carry = 0;
             for (size_t i = 0; i < length() || carry; i++) {
-                int64_t tmp = trhs.data[i];
+                int64_t tmp = trhs.m_data[i];
                 tmp -= carry;
-                tmp -= (i < length() ? data[i] : 0);
+                tmp -= (i < length() ? m_data[i] : 0);
                 carry = tmp < 0;
                 if (carry) tmp += ((int64_t) 1ll << 32);
-                trhs.data[i] = tmp;
+                trhs.m_data[i] = tmp;
             }
 
             sign = s2;
-            std::swap(data, trhs.data);
+            std::swap(m_data, trhs.m_data);
         }
     }
 
@@ -106,8 +118,8 @@ big_integer &big_integer::operator+=(big_integer const &rhs) {
 }
 
 void big_integer::trim_zeros() {
-    while (length() > 1 && data.back() == 0) {
-        data.pop_back();
+    while (length() > 1 && m_data.back() == 0) {
+        m_data.pop_back();
     }
 }
 
@@ -131,7 +143,7 @@ big_integer &big_integer::operator*=(const big_integer &rhs) {
         uint32_t carry = 0;
         for (int j = 0; j < rhs.length() || carry; j++) {
             uint64_t cur = tmp[i + j]
-                           + data[i] * ((uint64_t) 1) * (j < rhs.length() ? rhs.data[j] : 0)
+                           + m_data[i] * ((uint64_t) 1) * (j < rhs.length() ? rhs.m_data[j] : 0)
                            + carry;
 
             tmp[i + j] = (cur << 32) >> 32;
@@ -139,7 +151,7 @@ big_integer &big_integer::operator*=(const big_integer &rhs) {
         }
     }
 
-    std::swap(data, tmp);
+    std::swap(m_data, tmp);
     sign ^= rhs.sign;
     sign ^= 1;
 
@@ -161,7 +173,7 @@ big_integer abs(big_integer const &x) {
 std::pair<big_integer, big_integer> big_integer::div_mod(big_integer const &b) const {
     const uint32_t max_dig = 0xFFFFFFFF;
 
-    if (b.length() == 1 && b.data[0] == 0) {
+    if (b.length() == 1 && b.m_data[0] == 0) {
         throw std::runtime_error("Division by zero");
     }
 
@@ -169,7 +181,7 @@ std::pair<big_integer, big_integer> big_integer::div_mod(big_integer const &b) c
     tempr.sign ^= (b.sign ^ 1);
 
     if (b.length() == 1) {
-        auto k = tempr.div_mod(b.data[0]);
+        auto k = tempr.div_mod(b.m_data[0]);
         big_integer c2 = big_integer(k.second);
         if (!c2.is_zero()) {
             c2.sign = this->sign;
@@ -186,7 +198,7 @@ std::pair<big_integer, big_integer> big_integer::div_mod(big_integer const &b) c
         return std::make_pair(tempr, tempr); // with move or not??
     }
 
-    uint32_t f = (((uint64_t) 1) << 32) / ((uint64_t) b.data.back() + 1);
+    uint32_t f = (((uint64_t) 1) << 32) / ((uint64_t) b.m_data.back() + 1);
 
     abs_x *= f;
     abs_y *= f;
@@ -194,21 +206,21 @@ std::pair<big_integer, big_integer> big_integer::div_mod(big_integer const &b) c
     std::vector<uint32_t> result(abs_x.length() - abs_y.length() + 1);
 
     big_integer h;
-    uint64_t d1 = abs_y.data.back();
+    uint64_t d1 = abs_y.m_data.back();
 
     for (std::ptrdiff_t k = abs_x.length() - 1; k > abs_x.length() - abs_y.length(); k--) {
         h <<= 32;
-        h += abs_x.data[k];
+        h += abs_x.m_data[k];
     }
 
     for (size_t k = abs_x.length() - abs_y.length() + 1; k--;) {
         h <<= 32;
-        h += abs_x.data[k];
+        h += abs_x.m_data[k];
 
-        uint64_t r2 = h.data.back();
+        uint64_t r2 = h.m_data.back();
         if (h.length() > abs_y.length()) {
             r2 <<= 32;
-            r2 += h.data[h.length() - 2];
+            r2 += h.m_data[h.length() - 2];
         }
 
         //       quotient
@@ -222,13 +234,13 @@ std::pair<big_integer, big_integer> big_integer::div_mod(big_integer const &b) c
         h -= dq;
     }
 
-    tempr.data = std::move(result);
+    tempr.m_data = std::move(result);
     tempr.trim_zeros();
 
     h.sign = this->sign;
 
     return std::make_pair(std::move(tempr), std::move(h)); // with move or not??
-};
+}
 
 big_integer &big_integer::operator/=(big_integer const &b) {
     move(div_mod(b).first);
@@ -249,11 +261,11 @@ std::pair<big_integer, uint32_t> big_integer::div_mod(uint32_t dividend) const {
 
     res.second = 0;
     res.first.sign = sign;
-    res.first.data.resize(length());
+    res.first.m_data.resize(length());
 
     for (std::ptrdiff_t i = length() - 1; i >= 0; i--) {
-        uint64_t cur = data[i] + ((res.second * ((uint64_t) 1)) << 32);
-        res.first.data[i] = cur / dividend;
+        uint64_t cur = m_data[i] + ((res.second * ((uint64_t) 1)) << 32);
+        res.first.m_data[i] = cur / dividend;
         res.second = cur % dividend;
     }
 
@@ -305,25 +317,25 @@ big_integer &big_integer::operator^=(big_integer const &rhs) {
 
 big_integer &big_integer::operator<<=(int rhs) {
     big_integer tmp(*this);
-    tmp.data.resize(length() + rhs / 32 + 2);
+    tmp.m_data.resize(length() + rhs / 32 + 2);
 
     int c1 = rhs >> 5;
     int c2 = (rhs << 27) >> 27;
 
     if (c1) {
         for (std::ptrdiff_t i = length() + c1 - 1; i >= (rhs >> 5); i--) {
-            tmp.data[i] = tmp.data[i - c1];
+            tmp.m_data[i] = tmp.m_data[i - c1];
         }
 
         for (int i = (rhs >> 5) - 1; i >= 0; i--) {
-            tmp.data[i] = 0;
+            tmp.m_data[i] = 0;
         }
     }
 
     if (c2) {
         for (std::ptrdiff_t i = length() + c1 - 1; i >= 0; i--) {
-            tmp.data[i + 1] |= (tmp.data[i] >> (32 - c2));
-            tmp.data[i] <<= c2;
+            tmp.m_data[i + 1] |= (tmp.m_data[i] >> (32 - c2));
+            tmp.m_data[i] <<= c2;
         }
     }
 
@@ -408,11 +420,11 @@ big_integer big_integer::operator--(int) {
 }
 
 bool operator==(big_integer const &a, big_integer const &b) {
-    return a.sign == b.sign && a.data == b.data;
+    return a.sign == b.sign && a.m_data == b.m_data;
 }
 
 bool operator!=(big_integer const &a, big_integer const &b) {
-    return a.sign != b.sign || a.data != b.data;
+    return a.sign != b.sign || a.m_data != b.m_data;
 }
 
 bool operator<(big_integer const &a, big_integer const &b) {
@@ -420,8 +432,8 @@ bool operator<(big_integer const &a, big_integer const &b) {
     if (a.length() != b.length()) { return a.length() < b.length(); }
 
     for (std::ptrdiff_t i = a.length() - 1; i >= 0; i--) {
-        if (a.data[i] != b.data[i]) {
-            return a.data[i] < b.data[i];
+        if (a.m_data[i] != b.m_data[i]) {
+            return a.m_data[i] < b.m_data[i];
         }
     }
 
@@ -433,8 +445,8 @@ bool operator>(big_integer const &a, big_integer const &b) {
     if (a.length() != b.length()) { return a.length() > b.length(); }
 
     for (std::ptrdiff_t i = a.length() - 1; i >= 0; i--) {
-        if (a.data[i] != b.data[i]) {
-            return a.data[i] > b.data[i];
+        if (a.m_data[i] != b.m_data[i]) {
+            return a.m_data[i] > b.m_data[i];
         }
     }
 
@@ -446,8 +458,8 @@ bool operator<=(big_integer const &a, big_integer const &b) {
     if (a.length() != b.length()) { return a.length() < b.length(); }
 
     for (std::ptrdiff_t i = a.length() - 1; i >= 0; i--) {
-        if (a.data[i] != b.data[i]) {
-            return a.data[i] < b.data[i];
+        if (a.m_data[i] != b.m_data[i]) {
+            return a.m_data[i] < b.m_data[i];
         }
     }
 
@@ -459,8 +471,8 @@ bool operator>=(big_integer const &a, big_integer const &b) {
     if (a.length() != b.length()) { return a.length() > b.length(); }
 
     for (std::ptrdiff_t i = a.length() - 1; i >= 0; i--) {
-        if (a.data[i] != b.data[i]) {
-            return a.data[i] > b.data[i];
+        if (a.m_data[i] != b.m_data[i]) {
+            return a.m_data[i] > b.m_data[i];
         }
     }
 
@@ -495,7 +507,7 @@ std::ostream &operator<<(std::ostream &s, big_integer const &a) {
 }
 
 bool big_integer::is_zero() const {
-    return length() == 1 && data[0] == 0;
+    return length() == 1 && m_data[0] == 0;
 }
 
 big_integer operator+(big_integer a, big_integer const &b) {
@@ -548,44 +560,44 @@ big_integer operator>>(big_integer a, int b) {
 
 // integer length in radix 2^32, without sign
 size_t big_integer::length() const {
-    return data.size();
+    return m_data.size();
 }
 
 std::vector<uint32_t> big_integer::get_data() const {
-    return data;
+    return m_data;
 }
 
 std::vector<uint32_t> get_complement_data(big_integer const &x) {
     if (x.sign) {
-        return x.data;
+        return x.m_data;
     }
 
     big_integer tmp(x);
     tmp.sign = true;
 
     for (int i = 0; i < tmp.length(); i++) {
-        tmp.data[i] = ~tmp.data[i];
+        tmp.m_data[i] = ~tmp.m_data[i];
     }
 
     tmp++;
-    return std::move(tmp.data);
+    return std::move(tmp.m_data);
 }
 
 big_integer get_decomplement_data(std::vector<uint32_t> const &x) {
     if (x.back() & (1 << 31)) {
         big_integer tmp;
-        tmp.data = x;
+        tmp.m_data = x;
 
         tmp--;
         for (int i = 0; i < tmp.length(); i++) {
-            tmp.data[i] = ~tmp.data[i];
+            tmp.m_data[i] = ~tmp.m_data[i];
         }
 
         tmp.sign = false;
         return tmp;
     } else {
         big_integer tmp;
-        tmp.data = x;
+        tmp.m_data = x;
         return tmp;
     }
 }
@@ -614,7 +626,7 @@ big_integer apply_with_complement(big_integer const &x,
 }
 
 void big_integer::move(big_integer const& from) {
-    this->data = std::move(from.data);
+    this->m_data = std::move(from.m_data);
     this->sign = from.sign;
 }
 
